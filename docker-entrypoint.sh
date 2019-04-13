beed42a2f85776b1bd161e243b25d47774c90b43
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2019-04-14 01:59:11 +0530

    Modify docker container entrypoint for fully scripted deployment

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Commands to get feature_requester up and running.
+python manage.py drop_db
+python manage.py create_db
+python manage.py init_db
+python manage.py runserver
+
+exec "$@"
